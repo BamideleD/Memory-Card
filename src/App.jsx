3a52e9701864e2598945e379f1429ef82def8975
BamideleD <bamadboss@gmail.com>
2026-01-02 05:28:38 +0100

    Memory Card

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { GameHeader } from "./Components/GameHeader"
+
+
+function App() {
+return (
+  <div className="app">
+    <GameHeader score = {0} moves = {0} />
+  </div>
+);
+}
+
+export default App
